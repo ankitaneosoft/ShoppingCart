f87a690e34edc7714ea8df227dd44cf7c889336b
ankita <ankita.lahoti@wwindia.com>
2016-08-30 11:54:55 +0530

    added migration files

--- ShoppingCart/settings.py
+++ ShoppingCart/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-#import dj_database_url
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,26 +77,23 @@ TEMPLATE_DIRS = (
 )
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'shoppingcart_db',
+#         'USER': 'adminuser',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+#         'PORT': '',        
+#     }
+# }
+
+
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shoppingcart_db',
-        'USER': 'adminuser',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',        
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
-
-
-
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-# }
 
 
 
